45afc78cfe63622bb31406dc3df93a125b648c00
Dmitri Pisarev <dimaip@gmail.com>
2018-03-03 00:21:19 +0300

    TASK: adjust to the new UI

--- Classes/DataSources/AssetCollectionDataSource.php
+++ Classes/DataSources/AssetCollectionDataSource.php
@@ -54,10 +54,10 @@ class AssetCollectionDataSource extends AbstractDataSource
             /** @var \Neos\Media\Domain\Model\AssetCollection $assetCollection */
             $options[] = [
                 'label' => $assetCollection->getTitle(),
-                'value' => json_encode([
+                'value' => [
                     '__identity' => $this->persistenceManager->getIdentifierByObject($assetCollection),
                     '__type' => TypeHandling::getTypeForValue($assetCollection)
-                ])
+                ]
             ];
         }
 
